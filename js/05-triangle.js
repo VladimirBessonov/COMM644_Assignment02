e106c0d058ff3984949e3a36519c66bd2a08c937
Vladimir <2000.besson@gmail.com>
2020-02-20 07:25:47 -0800

    changed 05 task

--- js/05-triangle.js
+++ js/05-triangle.js
@@ -1,12 +1,45 @@
 var counter = 1
 var hashSymbol = '#';
 
-//
-// while ( counter < 6 ) {
-//     let hashArr = Array.apply(null, Array(counter)).map( () => {return hashSymbol});
-//     console.log(hashArr);
-//     counter++
-// }
+var size = prompt( 'Enter the size of the Pyramid to build up -> down and down -> up, default is 6', 6)
+
+function delay(n) {  
+    n = n || 2000;
+    return new Promise(done => {
+      setTimeout(() => {
+        done();
+      }, n);
+    });
+  }
+
+(async (size) => {
+    console.log('up -> donw')
+while ( counter <= size ) {
+    let hashArr = Array.apply(null, Array(counter)).map( () => {return hashSymbol});
+    console.log(hashArr);
+    await delay(1000)
+    counter++
+}
+console.log('down -> up')
+counter--
+while ( counter > 0 ) {
+    let hashArr = Array.apply(null, Array(counter)).map( () => {return hashSymbol});
+    console.log(hashArr);
+    await delay(1000)
+    counter--
+}
+})(size);
+
+
+// (async () => {
+//     while ( counter > 1 ) {
+//         let hashArr = Array.apply(null, Array(counter)).map( () => {return hashSymbol});
+//         console.log(hashArr);
+//         await delay(1000)
+//         counter--
+//     }
+//     })();
+
 
 // pyramide down
 
@@ -22,15 +55,15 @@ var hashSymbol = '#';
     // })(6);
 
 // pyramide up
-    (function whileLoop(n, m) {
-        setTimeout(function () {
-            let hashArr = Array.apply(null, Array(n)).map(() => {
-                return hashSymbol
-            });
-            console.log(hashArr);
-            if (n < m) {
-                ++n;
-                whileLoop(n, m);
-            }
-        }, 2000)
-    })(1, 6);
+    // (function whileLoop(n, m) {
+    //     setTimeout(function () {
+    //         let hashArr = Array.apply(null, Array(n)).map(() => {
+    //             return hashSymbol
+    //         });
+    //         console.log(hashArr);
+    //         if (n < m) {
+    //             ++n;
+    //             whileLoop(n, m);
+    //         }
+    //     }, 2000)
+    // })(1, 6);
